3a91ed6ad00b980646097fa128f9da257df32422
enamulhimel <mhasan173013@bscse.uiu.ac.bd>
2023-02-22 14:02:02 +0600

    build banner component

--- src/Banner.js
+++ src/Banner.js
@@ -27,26 +27,24 @@ function Banner() {
   return (
     <header className='banner' style={{
         backgroundSize:"cover",
-        backgroundImage:`url("https://wallpapercave.com/wp/wp2831956.png")`,
+        backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition:"center center",
     }}>
 
         <div className="banner-contents">
             <h1 className="banner-title">
-                Movie Name
+                {movie?.title || movie?.name || movie?.original_name} 
             </h1>
             <div className="banner-buttons">
                 <button className='banner-button'>Play</button>
                 <button className='banner-button'>My List</button>
             </div>
             <h1 className="banner-description">
-                {truncate(
-                    `This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description`,
-                    150)}
+                {truncate(movie?.overview,150)}
             </h1>
         </div>
 
-        <div className="banner-fadeButton" />
+        <div className="banner--fadeButton" />
 
     </header>
   )
