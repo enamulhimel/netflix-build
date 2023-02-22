a3858f018754e8e189d7840cd493a949045b49b5
enamulhimel <mhasan173013@bscse.uiu.ac.bd>
2023-02-22 23:43:37 +0600

    build row component

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Banner from './Banner';
 import './HomeScreen.css';
 import Nav from './Nav';
+import requests from './Requests';
+import Row from './Row';
 
 function HomeScreen() {
   return (
@@ -9,6 +11,21 @@ function HomeScreen() {
         <Nav />
 
         <Banner />
+
+        <Row
+          title="NETFLIX ORIGINALS"
+          fetchUrl={requests.fetchNetflixOriginals}
+          isLargeRow
+         />
+        <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
+        <Row title="TOP RATED" fetchUrl={requests.fetchTopRated} />
+        <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies} />
+        <Row title="COMEDY MOVIES" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="ROMANCE MOVIES" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="DOCUMENTARY movies" fetchUrl={requests.fetchDocumentaries} />
+
+        
 
     </div>
   )
